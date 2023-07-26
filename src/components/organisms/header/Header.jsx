067bd2f25c0dc7bae5bf189afb3af4349e501245
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-07-26 16:25:44 -0400

    layout advanced: header

--- src/components/organisms/header/Header.jsx
+++ src/components/organisms/header/Header.jsx
@@ -1,7 +1,20 @@
+import SearchBar from "../../atoms/SearchBar"
+import { Grid } from "@mui/material"
+import { Logo } from "../../atoms/Logo"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 export const Header = () => {
-  return (<>
-  
-  <h1>Este es el Header</h1>
-  </>
+  return (<Grid container>
+      
+
+    <Grid item sm={2}>
+      <Logo/>
+    </Grid>
+    <Grid item xs={6}>
+  <SearchBar/>
+    </Grid>
+    <Grid item xs={2}>
+      <ShoppingCartIcon  color="success"/>
+    </Grid>
+  </Grid>
   )
 }
