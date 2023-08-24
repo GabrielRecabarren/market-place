b8ff3b33cb629671d74962978117bb3bc1802569
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-08-24 17:52:55 -0400

    Use context for CartContext

--- src/components/molecules/Badge.jsx
+++ src/components/molecules/Badge.jsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import Badge from "@mui/material/Badge";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export default function SimpleBadge() {
+  const cartContext = React.useContext(CartContext);
   return (
-    <Badge badgeContent={4} color="success">
+    <Badge badgeContent={cartContext} color="success">
       <ShoppingCartIcon />
     </Badge>
   );
